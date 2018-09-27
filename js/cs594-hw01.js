97b499455244f40ceb8545fec83986c3594a54ee
vaibhav singh <vaibhav.singh21@yahoo.co.in>
2018-09-27 18:08:56 -0500

    added hw01

---
+++ js/cs594-hw01.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    // Bind SVG clicks
+    var circle = $("#svg-circle")
+    var rect = $("#svg-rect")
+    var poly = $("#svg-poly")
+
+    circle.on("click", function () {
+        circle.hide()
+        rect.show()
+    })
+
+    rect.on("click", function () {
+        rect.hide()
+        poly.show()
+    })
+
+    poly.on("click", function () {
+        poly.hide()
+        circle.show()
+    })
+});
